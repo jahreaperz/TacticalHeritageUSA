e4f026a8a2c155400e892cc491398288f32c17a9
jahreaperz <jahreaperz70@gmail.com>
2026-06-03 16:00:49 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+==================================================
+Tactical Heritage
+main.js
+
+Version: 1.0
+Purpose: Core Site Scripts
+==================================================
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('Tactical Heritage Initialized');
+
+});
